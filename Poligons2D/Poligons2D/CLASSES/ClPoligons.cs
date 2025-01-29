7804649dfb5d40e6742b07aab3548171bda83b86
Shavie5 <xavier.arechavalas@educem.net>
2025-01-29 17:14:02 +0100

    Pujada inicial del projecte

---
+++ Poligons2D/Poligons2D/CLASSES/ClPoligons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poligons2D.CLASSES
+{
+    public abstract class ClPoligons
+    {
+        //Propietats:
+        protected Form frmPare { get; set; }                    // Form on es dibuixarà el polígon
+        protected private Panel pnl { get; set; } = new Panel(); // panell dins el qual es dibuixa el polígon
+        protected Point posCentre { get; set; }                // posició del centre del Panel   
+        protected Color colorInterior { get; set; }             // color de l'interior
+        protected Boolean teInterior { get; set; }
+    }
+}
